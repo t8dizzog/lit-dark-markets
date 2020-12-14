45f61a3fc0b215d576ea1b1212083099e6247ce9
Matt Tait <matait@google.com>
2020-12-13 20:52:35 -0500

    Adding side input code (but commented out)

--- src/dataflow/normalize_forex.py
+++ src/dataflow/normalize_forex.py
@@ -7,6 +7,20 @@ import apache_beam.transforms.window as window
 
 from google.cloud import storage
 
+class EnrichCmeTrades(beam.DoFn):
+
+    def process(self, element, static_data):
+        import logging
+        curr_pair = static_data.get("product_code")
+        logging.info(f"static_data:\n {static_data}")
+        logging.info(f"curr_pair:\n {curr_pair}")
+        curr_pair_l = curr_pair.split("_")
+        element["currency_pair"]=curr_pair
+        element["currency_1"]=curr_pair_l[0]
+        element["currency_2"]=curr_pair_l[1]
+        logging.info(f"TLDR-NEW-DOFN: Enriched dict:\n {element}")
+        return element
+
 class ConvertToStandardFormat(beam.DoFn):
     
     def process(self, element):
@@ -14,23 +28,40 @@ class ConvertToStandardFormat(beam.DoFn):
         import json
         import logging
         from datetime import datetime
-    
-        logging.info(f"element.data: \n{element.data}")
-        logging.info(f"element: \n{element}")
-        logging.info(f"element.attributes: \n{element.attributes}")
 
-        payload =json.loads(element.data.decode("utf-8"))
-        trade_summary = payload.get("tradeSummary")
-        instrument = payload.get("instrument")
-        ts = int(element.attributes.get("SendingTime"))
+        #Get payload and its entries from PubSub message's data element
+        payload =json.loads(element.data.decode("utf-8")).get("payload")
+        trade_summary = payload[0].get("tradeSummary")
+        instrument = payload[0].get("instrument")
+        trade_timestamp = payload[0].get("lastUpdateTime")
+        
+        #Get payload attribute entries
+        ts = float(element.attributes.get("SendingTime"))/1000.0
         timestamp = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S.%f000')
-        logging.info(f"timestamp: \n{timestamp}")
-
+        
+        #Get payload trade summary entries
         trade_id = trade_summary.get("mdTradeEntryId")
-        logging.info(f"trade_id: \n{trade_id}")
-
+        aggressor = trade_summary.get("aggressorSide")
+        size = trade_summary.get("tradeQty")
+        price = trade_summary.get("tradePrice")
+        
+        #Get payload instrument entries
+        venue = instrument.get("exchangeMic")
+        symbol = instrument.get("symbol")
+        product_type = instrument.get("productType")
+        product_code = instrument.get("productCode")
+        
+        output_msg_dict = dict( zip(
+            ("trade_id","timestamp","venue","symbol","aggressor","seller","buyer",\
+            "trade_timestamp","size","price","product_type","product_code"),
+            (trade_id,timestamp,venue,symbol,aggressor,None,None,\
+            trade_timestamp,size,price,product_type,product_code)))
+        
+        logging.info(f"TLDR1: Output msg dict:\n {output_msg_dict}")
+        return output_msg_dict
+        
 class WriteBatchesToGCS(beam.DoFn):
-    # TODO: Attribution
+    # TODO: Make into my own
     def __init__(self, gcs_output_path, filename_prefix=""):
         self.gcs_output_path = gcs_output_path
         self.filename_prefix = filename_prefix
@@ -44,6 +75,7 @@ class WriteBatchesToGCS(beam.DoFn):
         window_start = window.start.to_utc_datetime().strftime(ts_format)
         window_end = window.end.to_utc_datetime().strftime(ts_format)
         mdy = window.start.to_utc_datetime().strftime(mdy_format)
+        # TODO: Make filename prefix a top-level folder, as well as a filename prefix
         filename = "/".join([self.gcs_output_path, mdy, f"{self.filename_prefix}{window_start}_{window_end}.txt"])
 
         with beam.io.gcp.gcsio.GcsIO().open(filename=filename, mode="w") as f:
@@ -52,7 +84,7 @@ class WriteBatchesToGCS(beam.DoFn):
 
 
 class MakeBatchesByWindow(beam.PTransform):
-    # TODO: Attribution
+    # TODO: Make into my own
     """A composite transform that groups Pub/Sub messages based on publish
     time and outputs a list of dictionaries, where each contains one message
     and its publish timestamp.
@@ -81,7 +113,7 @@ class MakeBatchesByWindow(beam.PTransform):
 
 
 class AddTimestamps(beam.DoFn):
-    #TODO: Attribution
+    #TODO: Make into my own
     import apache_beam as beam
     def process(self, element, publish_time=beam.DoFn.TimestampParam):
         """Processes each incoming windowed element by extracting the Pub/Sub
@@ -99,7 +131,7 @@ class AddTimestamps(beam.DoFn):
         }
 
 def run(config_bucket, input_subscription_json, otc_subscription, bq_output_table,
-        pubsub_output_topic, gcs_output_path, window_size=1.0, pipeline_args=None):
+        pubsub_output_topic, gcs_output_path, side_input_query, window_size=1.0, pipeline_args=None):
     # TODO Verify that save_main_session=True is required by DoFns which rely on modules imported into the global
     # namespace (but not the DF namespace)
 
@@ -108,6 +140,7 @@ def run(config_bucket, input_subscription_json, otc_subscription, bq_output_tabl
     )
 
     # Get currency futures subscriptions
+    #TODO: retrieve this using beam.io classes; not using gcs client classes
     gcs_client = storage.Client()
     bucket = gcs_client.get_bucket(config_bucket)
     json_object = bucket.blob(input_subscription_json)
@@ -115,7 +148,11 @@ def run(config_bucket, input_subscription_json, otc_subscription, bq_output_tabl
     subs = config_dict.get("futures_subscriptions")
     subs_dict = {}
 
+    #side_p = beam.Pipeline(options=pipeline_options)
     p = beam.Pipeline(options=pipeline_options)
+
+    #Get static product data as a side input
+    #mapping_p_coll = p | "Read product static data" >> beam.io.gcp.bigquery.ReadFromBigQuery(query=side_input_query)
 
     for sub in subs:
         key = sub.rsplit("-", 3)[1]
@@ -124,41 +161,20 @@ def run(config_bucket, input_subscription_json, otc_subscription, bq_output_tabl
         subs_dict[key] = p_coll
         print(f"Just added key {key}\n")
 
-    merged_pcolls = subs_dict.values() | "Merge forex trades subscriptions" >> beam.Flatten(
+    merged_pcolls = subs_dict.values() | "Merge FX futures trades subscriptions" >> beam.Flatten(
     )
     
     merged_pcolls | "Window into" >> MakeBatchesByWindow(window_size) | "Write to GCS" >> beam.ParDo(WriteBatchesToGCS(
         gcs_output_path,"trade_")) 
         
     merged_pcolls | "Convert to vendor-agnostic JSON trade format" >> beam.ParDo(
-        ConvertToStandardFormat()) 
+       ConvertToStandardFormat()) 
+    
+    #merged_pcolls | "Enrich with static data" >> beam.ParDo(EnrichCmeTrades(), data=beam.pvalue.AsDict(mapping_p_coll))
+        
 
     result = p.run()
     result.wait_until_finish()
-
-    # po = NormalizeForexOptions(flags=argv)
-    # po.view_as(StandardOptions).streaming = True
-    # p = Pipeline(options=po)
-
-    # TODO: Confirm that the _all_options bit is necessary. I shouldn't have to access
-    # a private dict
-    # project= p.options._all_options.get("project")
-
-    # table_path = p.options.output_table
-
-    # TODO: Validate that all expected fields exist
-    # Merge all PCollections
-    # merged_pcolls = subs_dict.values() | "merge input subscriptions" >> Flatten()
-
-    # Write messages to BigQuery
-    # merged_pcolls | Map(lamda x: bytes.decode(x)) |"write trades to BQ" >> beam.WriteToBigQuery(
-    #                                             # Write the messages
-    #                                             table_path,
-    #                                             write_disposition=beam.BigQueryDisposition.WRITE_APPEND)
-
-    # result = p.run()
-    # result.wait_until_finish()
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -198,11 +214,18 @@ parser.add_argument(
     help="Path for GCS audit output; include filename prefix (but not event type).\n",
 )
 parser.add_argument(
+    "--side_input_query",
+    type=str,
+    default="select symbol,currency_pair,cme_contract_size from `lit-dark-markets.fx.product_symbol`",
+    help="Query to get symbol-to-currency-pair mapping.\n",
+)
+parser.add_argument(
     "--window_size",
     type=float,
     default=1.0,
     help="Output file frequency in minutes.\n",
-)
+),
+
 known_args, pipeline_args = parser.parse_known_args()
 
 run(
@@ -212,6 +235,7 @@ run(
     known_args.bq_output_table,
     known_args.pubsub_output_topic,
     known_args.gcs_output_path,
+    known_args.side_input_query,
     known_args.window_size,
     pipeline_args,
 )
